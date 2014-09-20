6aa6bfa653f166b4e15f835ed26e3d7d2348142d
egor.a.trubniko.panov <trubnikov-ea@gmail.com>
2014-09-20 13:24:29 +0600

    start

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 $(function(){
+
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d');
+
+  var imgd = context.getImageData(x, y, width, height);
+  var pix = imgd.data;
+
+  // Loop over each pixel and invert the color.
+  for (var i = 0, n = pix.length; i < n; i += 4) {
+    pix[i  ] = 255 - pix[i  ]; // red
+    pix[i+1] = 255 - pix[i+1]; // green
+    pix[i+2] = 255 - pix[i+2]; // blue
+    // i+3 is alpha (the fourth element)
+  }
+
+  // Draw the ImageData at the given (x,y) coordinates.
+  context.putImageData(imgd, x, y);
+
+
 })();
