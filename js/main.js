599ea4d90f43f7a2a3f67fe1567f055ce4f289a4
Alexander Serditov <av.serditov@gmail.com>
2014-09-20 22:09:37 +0600

    add tools for changing size of segment and url of image

--- js/main.js
+++ js/main.js
@@ -30,6 +30,7 @@ $(function(){
                 'style': 'top: ' + y + 'px; left: ' + x + 'px;'
             }));
         },
+        // HELPERS START
         rgbToHex: function(r, g, b) {
             if (r > 255 || g > 255 || b > 255)
                 throw "Invalid color component";
@@ -47,8 +48,7 @@ $(function(){
                 B: b
             };
         },
-        getNearestColor: function(hex)
-        {
+        getNearestColor: function(hex) {
             var n = 12;
             var distance = Number.MAX_VALUE;
 
@@ -67,57 +67,70 @@ $(function(){
                 (hex1.R-hex2.R)*(hex1.R-hex2.R)+(hex1.B-hex2.G)*(hex1.G-hex2.G)+(hex1.B-hex2.B)*(hex1.B-hex2.B)
             );
         },
-        getMapImage: function ()
-        {
-            var step = SEGMENT_SIZE;
-            var image_w, image_h;
-            var canvas = document.getElementById("myCanvas");
-          canvas.width = image_w = $('#myImage').width();//ширина
-          canvas.height = image_h = $('#myImage').height();//высота
-
-          var ctx = canvas.getContext('2d');
-          var img = new Image();  // Создание нового объекта изображения
-          img.src = $('#myImage').attr('src');
-          var color = [];
-          img.onload = $.proxy(function ()
-          {    // Событие onLoad, ждём момента пока загрузится изображение
-            ctx.drawImage(img, 0, 0);  // Рисуем изображение от точки с координатами 0, 0
-
-            var s = "rgb(", r = 0, g = 0, b = 0, f = ")", z = ',';
-            var imgd;
-            var pix;
-            var p_color = [];
-
-            for (var i = 0, h = image_h; i < h; i += step) {
-
-              for (var j = 0, w = image_w; j < w; j += step) {
-
-                imgd = ctx.getImageData(j, i, step, step);
-                pix = imgd.data;
-
-
-                for (var ii = 0, n = pix.length; ii < n; ii += 4) {
-
-                  r += pix[ii]; // red
-                  g += pix[ii + 1]; // green
-                  b += pix[ii + 2]; // blue
-
-                }
-                r = r / n;
-                g = g / n;
-                b = b / n;
-
-                p_color.push('#' + this.rgbToHex(r, g, b))
-
-              }
-              color.push(p_color);
-              p_color = [];
-
-            }
-            this.drawImage(color);
-          }, this);
+        // HELPERS END
+        init: function(){
+            $('#id_urlImage').val($('#myImage').attr('src'));
+            this.initEvents();
+            this.initImage();
         },
-        drawImage: function(list){
+        initImage: function(){
+            var canvas = document.getElementById("myCanvas");
+            var $image = $('#myImage');
+
+            var img = new Image();  // Создание нового объекта изображения
+            img.src = $image.attr('src');
+            img.onload = $.proxy(function () { // Событие onLoad, ждём момента пока загрузится изображение
+                canvas.width = $image.width(); // ширина
+                canvas.height= $image.height(); // высота
+                this.drawSuperImage(this.getImageMap(canvas, img));
+            }, this);
+        },
+        initEvents: function(){
+            $('#id_updateImage').on('click', $.proxy(this.onClickUpdateImage, this));
+            $('#id_form_updateImage').on('submit', $.proxy(this.onClickUpdateImage, this));
+        },
+        onClickUpdateImage: function(e){
+            e.preventDefault();
+            var src = $('#id_urlImage').val();
+            $('#myImage').attr('src', src);
+            $('.workspace__map-inner').empty();
+            SEGMENT_SIZE = $('#id_segmentSize').val()*1;
+            this.initImage();
+        },
+        getImageMap: function(canvas, img) {
+            var imgd, pix,
+                r = 0, g = 0, b = 0,
+                rowSegments = [];
+
+            var ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+
+            for (var i = 0, h = $(canvas).height(); i < h; i += SEGMENT_SIZE) {
+                var colSegments = [];
+
+                for (var j = 0, w = $(canvas).width(); j < w; j += SEGMENT_SIZE) {
+
+                    imgd = ctx.getImageData(j, i, SEGMENT_SIZE, SEGMENT_SIZE);
+                    pix = imgd.data;
+
+                    for (var ii = 0, n = pix.length; ii < n; ii += 4) {
+
+                        r += pix[ii]; // red
+                        g += pix[ii + 1]; // green
+                        b += pix[ii + 2]; // blue
+
+                    }
+                    r = r / n;
+                    g = g / n;
+                    b = b / n;
+
+                    colSegments.push('#' + this.rgbToHex(r, g, b))
+                }
+                rowSegments.push(colSegments);
+            }
+            return rowSegments;
+        },
+        drawSuperImage: function(list){
             for(var i = 0; i < list.length; i++) {
                 for(var j = 0; j < list[i].length; j++) {
                     var posX = SEGMENT_SIZE * j;
@@ -125,8 +138,8 @@ $(function(){
                     this.movePointToImage(posX, posY, this.getNearestColor(list[i][j]));
                 }
             }
+            $('#myCanvas').css({'visibility': 'hidden'})
         }
     };
-    app.getMapImage();
-
+    app.init();
 });
